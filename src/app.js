135ffcdfb7a6ee1904e8615f7b88c7a09b17ec5e
Pulkiiiit <pulkitshukla001@gmail.com>
2026-02-05 22:12:59 +0530

    feat: add cookie-parser middleware and utility classes for API error and response handling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import router from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+    
 
 app.use("/api/users", router);
 
